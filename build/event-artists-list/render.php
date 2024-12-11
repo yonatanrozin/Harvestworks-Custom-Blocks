2da50255e83d8034d9f3c591e8f80c0be67ac40e
yonatanrozin <yonatanrozin42@gmail.com>
2024-12-11 13:29:43 -0500

    add artist hyperlinks

--- build/event-artists-list/render.php
+++ build/event-artists-list/render.php
@@ -1,18 +1,15 @@
 <div <?= get_block_wrapper_attributes() ?>>
     <?php foreach (get_field('listed_artists') as $post): ?>
+        <?php $post = get_post($post); ?>
         <div class="artist_card">
             <?php if (has_post_thumbnail($post)): ?>
                 <img src=<?= get_the_post_thumbnail_url($post) ?> />
             <?php endif; ?>
             <div class="artist_details" >
-                <h2><?= $post->post_title ?></h2>
+                <a href="<?= get_permalink($post) ?>"><h2><?= $post->post_title ?></h2></a>
                 <h3><?= get_field("tagline", $post->ID) ?></h3>
                 <p ><?= $post->post_excerpt ?></p>
             </div>
         </div>
     <?php endforeach; ?>
 </div>
-
-
-
-
