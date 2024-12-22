ebd00f2421a3505fecd9a26e753fd71aad6bdef9
alextyang <koopakondra@gmail.com>
2024-12-22 10:07:06 -0500

    nav - increment version

--- build/navigation-sidebar/index.asset.php
+++ build/navigation-sidebar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '10e7bebcf95e3b54171b');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b3bf6d0313ed5a164e9d');
