d3c546151cc046b1c617f756740e2724f4d17b2e
alextyang <koopakondra@gmail.com>
2024-12-29 12:34:40 -0500

    overlay - gave up on custom rendering

--- build/navigation-overlay/render.php
+++ build/navigation-overlay/render.php
@@ -4,13 +4,6 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
-
-$menu_items = wp_get_nav_menu_items(4);
-
-// Fetch children for each menu item, if possible
-foreach ($menu_items as $menu_item) {
-}
-
 ?>
 
 
@@ -19,9 +12,5 @@ foreach ($menu_items as $menu_item) {
 		<div className='nav-overlay-button-circle'></div>
 		<div className='nav-overlay-button-circle'></div>
 		<div className='nav-overlay-button-circle'></div>
-		<?php echo '<script>console.log(' .
-			var_dump($menu_items) . ')</script>' ?>
-		<?php echo '<script>console.log(' .
-			($menu_items) . ')</script>' ?>
 	</div>
 </div>
