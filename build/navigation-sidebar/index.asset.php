01ff779d922da6aef30770e72fe69057f6ffcf93
alextyang <koopakondra@gmail.com>
2024-12-28 20:20:16 -0500

    style: simplify sidebar support and adjust margins for improved layout

--- build/navigation-sidebar/index.asset.php
+++ build/navigation-sidebar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b3bf6d0313ed5a164e9d');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a74ff14a19a1da6a605e');
