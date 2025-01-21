0c4a7d38580d86b10d30cc1d3d5e5c1741078533
alextyang <koopakondra@gmail.com>
2025-01-21 12:20:13 -0500

    event info - output event fields for inspection

--- build/event-information/render.php
+++ build/event-information/render.php
@@ -11,6 +11,6 @@ if (function_exists('get_fields')) {
 
 <div <?php echo get_block_wrapper_attributes(); ?>>
 	<div class='event-information'>
-		<?php echo $fields ?>
+		<?php echo var_dump($fields) ?>
 	</div>
 </div>
