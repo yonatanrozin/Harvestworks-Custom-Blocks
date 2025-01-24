a85def8b25298f26d7110415c7061d098e6b2c22
alextyang <koopakondra@gmail.com>
2025-01-24 15:18:20 -0500

    event info - changed artist name markup from h1 to p for improved semantic structure and updated styles for better visual hierarchy

--- src/event-artists-list/render.php
+++ src/event-artists-list/render.php
@@ -6,7 +6,7 @@
                 <img src=<?= get_the_post_thumbnail_url($post) ?> />
             <?php endif; ?>
             <div class="artist_details">
-                <h1 class="artist_name"><?= $post->post_title ?></h1>
+                <p class="artist_name"><?= $post->post_title ?></p>
                 <p class="artist_bio"><?= $post->post_excerpt ?></p>
             </div>
         </a>
