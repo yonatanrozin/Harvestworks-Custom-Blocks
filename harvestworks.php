f954c7cf24a66e536631119fee851f51bf974926
alextyang <koopakondra@gmail.com>
2025-01-27 16:43:37 -0500

    project info - add project information block with necessary assets and styles

--- harvestworks.php
+++ harvestworks.php
@@ -31,6 +31,7 @@ function harvestworks_harvestworks_block_init()
 	register_block_type(__DIR__ . '/build/event-list');
 	register_block_type(__DIR__ . '/build/events-carousel');
 	register_block_type(__DIR__ . '/build/event-information');
+	register_block_type(__DIR__ . '/build/project-information');
 	register_block_type(__DIR__ . '/build/artist-information');
 	register_block_type(__DIR__ . '/build/logo-people');
 	register_block_type(__DIR__ . '/build/navigation-sidebar');
